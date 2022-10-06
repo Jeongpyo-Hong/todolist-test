266c874fd2e4003b0774f41377e94240abe34eba
Jeongpyo Hong <112886992+Jeongpyo-Hong@users.noreply.github.com>
2022-10-06 11:56:21 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
+let number = 1;
 function App() {
+  const [todos, setTodos] = useState({
+    id: 1,
+    title: "리액트를 배워봅시다",
+  });
+
+  const initialValue = {
+    id: 0,
+    title: "",
+  };
+
+  const [todo, setTodo] = useState({ initialValue });
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setTodo({
+      ...todo,
+      [name]: value,
+    });
+  };
+
+  const onsubmitHandler = (e) => {
+    e.preventDefault();
+    if (todo.title.trim() === "") return;
+    setTodos([...todos, { ...todo, id: number }]);
+    setTodo(initialValue);
+    number++;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={onsubmitHandler} className="form">
+        <input
+          name="title"
+          value={todo.title}
+          required
+          onChange={onChangeHandler}
+          type="text"
+        />
+        <button>추가하기</button>
+      </form>
+      <div className="title">
+        <p>TODO LIST</p>
+      </div>
+      <div className="todo-wrapper">
+        <div className="todo">
+          <p>{todo.title}</p>
+        </div>
+      </div>
+    </>
   );
 }
 
